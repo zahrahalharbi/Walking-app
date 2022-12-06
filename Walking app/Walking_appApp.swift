8e867cdb7b9a1f618978ac0874157d29877bbd40
zahrahalharbi <zaalharbi22@twq.idserve.net>
2022-12-06 08:49:44 +0300

    Initial Commit

---
+++ Walking app/Walking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walking_appApp.swift
+//  Walking app
+//
+//  Created by Zahrah. on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Walking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
